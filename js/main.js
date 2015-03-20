a01b43147c9bac4e827321d1bab5a57f9fb1fac3
copernicus-mogley <ryan@9wizards.com>
2015-03-19 20:10:02 -0700

    Added select text function

--- js/main.js
+++ js/main.js
@@ -51,3 +51,15 @@ function tapDidOccur (e) {
 	var cointap = e.target.parentElement.children;
 	cointap[2].style.display = (cointap[2].style.display == "none") ? "block" : "none";
 }
+// SELECT TEXT ON SINGLE CLICK
+function selectText(containerid) {
+	if (document.selection) {
+	    var range = document.body.createTextRange();
+	    range.moveToElementText(document.getElementById(containerid));
+	    range.select();
+	} else if (window.getSelection) {
+	    var range = document.createRange();
+	    range.selectNode(document.getElementById(containerid));
+	    window.getSelection().addRange(range);
+	}
+}
