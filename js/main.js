f283cbcc0a13022c46d13d4c2b6501e870a7d3f5
copernicus-mogley <ryan@9wizards.com>
2015-04-14 14:35:50 -0700

    Crowdfunding goals and received indicators

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 var fadeTimer = 200;
+var BTCRec;
+var BTCUSD;
 // DOCUMENT READY JQUERY STUFF
 $(document).ready(function(){
 	var FLOaddress = document.getElementById('alexandriaFLO').innerHTML;
@@ -16,8 +18,51 @@ $(document).ready(function(){
 				$('#lightbox').fadeOut(fadeTimer);
 			}
 		}
-	});	
+	});
+
+// FUNDING + CRYPTO
+	var daysLeft = Math.round(46 - ((((Date.parse(new Date()) - 1429038124000)/100)/60)/60)/24);
+	document.getElementById('daysleft').innerHTML = daysLeft;
+
+	$.ajax({
+	    url: 'https://api.blocktrail.com/v1/BTC/address/1AaMehNHLRDH4JPKaCkBjn8WFjJ8nEwvsr?api_key=d25b4b06d87b30b419ecbcf5bcc8c63fda3ecd49',
+	    success: function(e) {
+	    	console.info(e);
+	    	BTCRec = e.received;
+	    	document.getElementById('btc-received').innerHTML = 'Total Received: '+BTCRec/100000000+' BTC';
+	    	getBTCUSD();
+	    }
+	});
 });
+
+function getBTCUSD() {
+	$.ajax({
+	    url: 'http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=2',
+	    type: 'GET',
+	    success: function(e) {
+			var el = $( '#sketchpad' );
+			el.html(e.responseText);
+			var data = $.parseJSON($(el).html());
+			BTCUSD = parseFloat(data.return.markets.BTC.lasttradeprice);
+			populateProgress();
+			$('.btc-usd .btc-usd-output').text(Math.round((1/BTCUSD)*100000000)/100000000);
+		}
+	});
+}
+
+function populateProgress() {
+	var BTCfunded = BTCRec/100000000;
+	var FLOfunded = 0;
+	var PayPalfunded = 0;
+	var fundGoal = 20000;
+	var funded = BTCfunded + FLOfunded + PayPalfunded;
+	var fundedToGoal = (funded/fundGoal)*100;
+	var fundedDisplay = Math.round(fundedToGoal*100)/100;
+	console.log(BTCfunded +' '+ FLOfunded+' '+ PayPalfunded+' '+ fundGoal + ' ' + funded + ' ' + fundedToGoal);
+	$('#fund-progress-wrap').prepend('<span>'+ fundedDisplay +'%</span>');
+	$('#fund-progress-bar').width(fundedToGoal+'%');
+}
+
 // SELECTABLE TEXT
 function selectText(containerid) {
 	if (document.selection) {
