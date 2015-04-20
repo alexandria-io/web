95fe612341fa5f7f4563a96c651d10010599127e
copernicus-mogley <ryan@9wizards.com>
2015-04-20 12:52:06 -0700

    Ajax function rewrite

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 var fadeTimer = 200;
 var BTCRec;
 var BTCUSD;
+var FLOCost;
+var FLOLTC;
+var LTCUSD;
+var FLOUSD;
+
+var BittrexFLO;
+
 // DOCUMENT READY JQUERY STUFF
 $(document).ready(function(){
 	var FLOaddress = document.getElementById('alexandriaFLO').innerHTML;
@@ -21,40 +28,51 @@ $(document).ready(function(){
 	});
 
 // FUNDING + CRYPTO
-	var daysLeft = Math.round(46 - ((((Date.parse(new Date()) - 1429038124000)/1000)/60)/60)/24);
+	var daysLeft = Math.round(47 - ((((Date.parse(new Date()) - 1429038124000)/1000)/60)/60)/24);
 	document.getElementById('daysleft').innerHTML = daysLeft;
-
-	$.ajax({
-	    url: 'https://api.blocktrail.com/v1/BTC/address/1AaMehNHLRDH4JPKaCkBjn8WFjJ8nEwvsr?api_key=d25b4b06d87b30b419ecbcf5bcc8c63fda3ecd49',
-	    success: function(e) {
-	    	console.info(e);
-	    	BTCRec = e.received;
-	    	document.getElementById('btc-received').innerHTML = 'Total Received: '+BTCRec/100000000+' BTC';
-	    	getBTCUSD();
-	    }
+	
+	$.get('https://api.blocktrail.com/v1/BTC/address/1AaMehNHLRDH4JPKaCkBjn8WFjJ8nEwvsr?api_key=d25b4b06d87b30b419ecbcf5bcc8c63fda3ecd49').fail(function (response, message, xhr) {
+		displayWarning();
+	}).done(function (e, message, xhr) {
+		var el = $( '#sketchpad' );
+		el.html(e.results[0]);
+		var data = $.parseJSON($(el).html());
+    	BTCRec = data.received;
+    	document.getElementById('btc-received').innerHTML = 'Total Received: '+BTCRec/100000000+' BTC';
+    	getBTCUSD();
 	});
 });
 
 function getBTCUSD() {
-	$.ajax({
-	    url: 'http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=2',
-	    type: 'GET',
-	    success: function(e) {
-			var el = $( '#sketchpad' );
-			el.html(e.responseText);
-			var data = $.parseJSON($(el).html());
-			BTCUSD = parseFloat(data.return.markets.BTC.lasttradeprice);
-			populateProgress();
-			$('.btc-usd .btc-usd-output').text(Math.round((1/BTCUSD)*100000000)/100000000);
-		}
+	$.get('https://api.bitcoinaverage.com/ticker/global/USD/').fail(function (response, message, xhr) {
+		displayWarning();
+	}).done(function (e, message, xhr) {
+		var el = $( '#sketchpad' );
+		el.html(e.results[0]);
+		var data = $.parseJSON($(el).html());
+		BTCUSD = data.last;
+		getBittrexLast();
+		$('.btc-usd .btc-usd-output').text(Math.round((1/BTCUSD)*100000000)/100000000);
+	});
+}
+
+function getBittrexLast() {
+	$.get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-flo').fail(function (response, message, xhr) {
+	    displayWarning();
+	}).done(function (e, message, xhr) {
+		var el = $( '#sketchpad' );
+		el.html(e.results[0]);
+		BittrexFLO = $.parseJSON($(el).html());
+		populateProgress();
+		
 	});
 }
 
 function populateProgress() {
-	var BTCfunded = BTCRec/100000000;
-	var FLOfunded = 0;
-	var PayPalfunded = 0;
-	var fundGoal = 24000;
+	var BTCfunded = (BTCRec/100000000)*BTCUSD;
+	var FLOfunded = (200.8/100000000)*BittrexFLO.result[0].Last;
+	var PayPalfunded = 245;
+	var fundGoal = 36000;
 	var funded = BTCfunded + FLOfunded + PayPalfunded;
 	var fundedToGoal = (funded/fundGoal)*100;
 	var fundedDisplay = Math.round(fundedToGoal*100)/100;
@@ -99,5 +117,10 @@ function loadQR(obj) {
 		colorLight : "#FFFFFF",
 		correctLevel : QRCode.CorrectLevel.H
 	});
+	$('#lightbox').fadeIn(fadeTimer);
+}
+
+function lightbox(obj) {
+	$('#lightbox').html('<div id="lightbox-img"><img src="'+obj+'" /></div>');
 	$('#lightbox').fadeIn(fadeTimer);
 }
